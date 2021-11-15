b219028c29382dd8ce0e76e1ac58b550cbf6d808
Tonni Akter <tonniakterbithi@gmaile.com>
2021-11-15 13:37:36 +0600

    coding

---
+++ coding-club.js
@@ -0,0 +1,546 @@
+[
+    {
+      "img": "https://i.ibb.co/gjwSMCn/2.png",
+      "title": "The Creative Javascript",
+      "desc": "From Beginner Javascript To Building Awesome Web Application",
+      "price": 133,
+      "rating": 4.2,
+      "review": 450
+    },
+    {
+      "img": " https://i.ibb.co/23gP8Jx/3.png",
+      "title": "The Creative HTML5 & CSS3",
+      "desc": "Create 3 amazing, well designed and animated websi",
+      "price": 18.3,
+      "rating": 4,
+      "review": 400
+    },
+    {
+      "img": "https://i.ibb.co/xhDXHPP/4.png",
+      "title": "The Creative React and React Native ",
+      "desc": "From Beginner Javascript To Building Awesome Web Application",
+      "price": 133,
+      "rating": 3.2,
+      "review": 450
+    },
+    {
+      "img": "https://i.ibb.co/RpwvfwR/5.png",
+      "title": "Top 14 Big Data Certificate",
+      "desc": "Big Data is now growing as an opportunity rather than anything",
+      "price": 113,
+      "rating": 2.5,
+      "review": 300
+    },
+    {
+      "img": "https://i.ibb.co/gRJsX4R/6.jpg",
+      "title": "What is Bytecode in Java",
+      "desc": "Java gets executed. One such concept is Bytecode",
+      "price": 78,
+      "rating": 2.5,
+      "review": 200
+    },
+    {
+      "img": "https://i.ibb.co/X8hfFh7/7.png",
+      "title": "Array Of Objects In Java:",
+      "desc": "Java is a programming language that deals in object",
+      "price": 115,
+      "rating": 3.5,
+      "review": 100
+    },
+    {
+      "img": "https://i.ibb.co/prs78zc/8.png",
+      "title": "The Creative Javascript",
+      "desc": "From Beginner Javascript To Building Awesome Web Application",
+      "price": 42.29,
+      "rating": 3.5,
+      "review": 450
+    },
+    {
+      "img": "https://i.ibb.co/MRj46W3/9.jpg",
+      "title": "Top 100+ Python Interview",
+      "desc": "python gets executed. One such concept is Bytecode",
+      "price": 47.69,
+      "rating": 4.5,
+      "review": 500
+    },
+    {
+      "img": "https://i.ibb.co/qBZ1qYs/10.jpg",
+      "title": "Full Stack Web Development",
+      "desc": "Build a complete web product & Earn an Coding Club",
+      "price": 48.69,
+      "rating": 4,
+      "review": 500
+    },
+    {
+      "img": "https://i.ibb.co/qBZ1qYs/10.jpg",
+      "title": "Advanced Database Queries",
+      "desc": "Learn the fundamentals of interacting with relatio",
+      "price": 133,
+      "rating": 4.2,
+      "review": 450
+    },
+    {
+      "img": "https://i.ibb.co/94KSR5h/11.png",
+      "title": "Cloud Application Develop",
+      "desc": "There is an app for everything, from online shopping",
+      "price": 176.4,
+      "rating": 4.8,
+      "review": 736
+    },
+    {
+      "img": "https://i.ibb.co/xH3ZJbt/12.jpg",
+      "title": "UX Design and Evaluation",
+      "desc": "It is hard to overstate the importance of UX",
+      "price": 1794.6,
+      "rating": 4.2,
+      "review": 450
+    },
+    {
+      "img": "https://i.ibb.co/QP67CjC/13.jpg",
+      "title": "Programming for the Web",
+      "desc": "JavaScript is the programming language of the World",
+      "price": 149,
+      "rating": 5,
+      "review": 862
+    },
+    {
+      "img": "https://i.ibb.co/9g2GTRG/15.jpg",
+      "title": "HTML5 Apps and Game",
+      "desc": "Want to learn advanced HTML5 tips and techniques?",
+      "price": 199,
+      "rating": 4.7,
+      "review": 862
+    },
+    {
+      "img": "https://i.ibb.co/C0BkBQf/16.jpg",
+      "title": "Building Modern Nodejs",
+      "desc": "In modern cloud native application development, it",
+      "price": 169,
+      "rating": 4.8,
+      "review": 286
+    },
+    {
+      "img": "https://i.ibb.co/pyxfzLg/17.jpg",
+      "title": "Introduction to Cloud Dev",
+      "desc": "Please Note: Learners who successfully complete",
+      "price": 49,
+      "rating": 4.5,
+      "review": 550
+    },
+    {
+      "img": "https://i.ibb.co/WG3f3G9/18.jpg",
+      "title": "Computer Science for Web",
+      "desc": "The web is a crucial part of our everyday lives.",
+      "price": 313.2,
+      "rating": 3.5,
+      "review": 280
+    },
+    {
+      "img": "https://i.ibb.co/ZdGjnjb/19.jpg",
+      "title": "Web Application Technology",
+      "desc": "In this course, you'll explore the basic structure",
+      "price": 49,
+      "rating": 2.2,
+      "review": 320
+    },
+    {
+      "img": "https://i.ibb.co/6nGYp4s/20.png",
+      "title": "Django for Everybody",
+      "desc": "This series introduces Python programmers to build",
+      "price": 176.4,
+      "rating": 3.5,
+      "review": 650
+    },
+    {
+      "img": "https://i.ibb.co/L96v3rt/21.png",
+      "title": "Programming for the Web",
+      "desc": "Learn how to develop dynamic, interactive",
+      "price": 113,
+      "rating": 3.5,
+      "review": 120
+    },
+    {
+      "img": "https://i.ibb.co/ThVZ42z/22.png",
+      "title": "Using JavaScript, JQuery,",
+      "desc": "This fourth and final course in Django for Everybody",
+      "price": 314.79,
+      "rating": 4.5,
+      "review": 450
+    },
+    {
+      "img": "https://i.ibb.co/sWXkDWq/23.png",
+      "title": "Exploring GraphQL: A Quer",
+      "desc": "Learn about GraphQL, an alternative to REST,",
+      "price": 188.9,
+      "rating": 4.2,
+      "review": 450
+    },
+    {
+      "img": "https://i.ibb.co/6ZSGRnZ/24.jpg",
+      "title": "Cloud Application Develop",
+      "desc": "You will understand the differences between Public",
+      "price": 128.69,
+      "rating": 3.5,
+      "review": 150
+    },
+    {
+      "img": "https://i.ibb.co/XtWGtFp/25.png",
+      "title": "HTML5 Apps and Games",
+      "desc": "Today, developers are increasingly moving from nat",
+      "price": 113,
+      "rating": 3,
+      "review": 290
+    },
+    {
+      "img": "https://i.ibb.co/gjwSMCn/2.png",
+      "title": "The Creative React",
+      "desc": "Build 3 Awesome Web Applications With React!",
+      "price": 29.99,
+      "rating": 4,
+      "review": 320
+    },
+    {
+      "img": "https://i.ibb.co/23gP8Jx/3.png",
+      "title": "The Creative Javascrip",
+      "desc": "From Beginner Javascript To Building Awesome Web Application",
+      "price": 133,
+      "rating": 4.2,
+      "review": 450
+    },
+    {
+      "img": "https://i.ibb.co/X8hfFh7/7.png",
+      "title": "The Creative HTML5 & CSS3",
+      "desc": "Create 3 amazing, well designed and animated website",
+      "price": 18.9,
+      "rating": 4,
+      "review": 680
+    },
+    {
+      "img": "https://i.ibb.co/xhDXHPP/4.png",
+      "title": "The Creative React and Re",
+      "desc": "Build 3 Awesome Web Applications With React!",
+      "price": 133,
+      "rating": 4.2,
+      "review": 450
+    },
+    {
+      "img": "https://i.ibb.co/RpwvfwR/5.png",
+      "title": "Top 100+ Python Interview",
+      "desc": "python gets executed. One such concept is Bytecode",
+      "price": 123,
+      "rating": 4.2,
+      "review": 450
+    },
+    {
+      "img": "https://i.ibb.co/60TN8fb/31.jpg",
+      "title": "Full Stack Web Development",
+      "desc": "Build a complete web product & Earn an Coding Club",
+      "price": 13,
+      "rating": 3.5,
+      "review": 670
+    },
+    {
+      "img": "https://i.ibb.co/T1Yr6NF/32.jpg",
+      "title": "Advanced Database Queries",
+      "desc": "Learn the fundamentals of interacting with relatioon",
+      "price": 133,
+      "rating": 4.2,
+      "review": 450
+    },
+    {
+      "img": "https://i.ibb.co/VW3LYc0/33.png",
+      "title": "Cloud Application Develop",
+      "desc": "There is an app for everything, from online shoppi",
+      "price": 133,
+      "rating": 4.2,
+      "review": 450
+    },
+    {
+      "img": "https://i.ibb.co/c3BvCpP/34.png",
+      "title": "UX Design and Evaluation",
+      "desc": "Programming for the Web",
+      "price": 113,
+      "rating": 5,
+      "review": 630
+    },
+    {
+      "img": "https://i.ibb.co/xhDXHPP/4.png",
+      "title": "CS50's Computer Science f",
+      "desc": "From Beginner Javascript To Building Awesome Web Application",
+      "price": 133,
+      "rating": 4.2,
+      "review": 450
+    },
+    {
+      "img": "https://i.ibb.co/gjwSMCn/2.png",
+      "title": "The Creative Javascript",
+      "desc": "From Beginner Javascript To Building Awesome Web Application",
+      "price": 133,
+      "rating": 4.2,
+      "review": 450
+    },
+    {
+      "img": " https://i.ibb.co/23gP8Jx/3.png",
+      "title": "The Creative HTML5 & CSS3",
+      "desc": "Create 3 amazing, well designed and animated websi",
+      "price": 18.3,
+      "rating": 4,
+      "review": 400
+    },
+    {
+      "img": "https://i.ibb.co/xhDXHPP/4.png",
+      "title": "The Creative React and React Native ",
+      "desc": "From Beginner Javascript To Building Awesome Web Application",
+      "price": 133,
+      "rating": 3.2,
+      "review": 450
+    },
+    {
+      "img": "https://i.ibb.co/RpwvfwR/5.png",
+      "title": "Top 14 Big Data Certificate",
+      "desc": "Big Data is now growing as an opportunity rather than anything",
+      "price": 113,
+      "rating": 2.5,
+      "review": 300
+    },
+    {
+      "img": "https://i.ibb.co/gRJsX4R/6.jpg",
+      "title": "What is Bytecode in Java",
+      "desc": "Java gets executed. One such concept is Bytecode",
+      "price": 78,
+      "rating": 2.5,
+      "review": 200
+    },
+    {
+      "img": "https://i.ibb.co/X8hfFh7/7.png",
+      "title": "Array Of Objects In Java:",
+      "desc": "Java is a programming language that deals in object",
+      "price": 115,
+      "rating": 3.5,
+      "review": 100
+    },
+    {
+      "img": "https://i.ibb.co/prs78zc/8.png",
+      "title": "The Creative Javascript",
+      "desc": "From Beginner Javascript To Building Awesome Web Application",
+      "price": 42.29,
+      "rating": 3.5,
+      "review": 450
+    },
+    {
+      "img": "https://i.ibb.co/MRj46W3/9.jpg",
+      "title": "Top 100+ Python Interview",
+      "desc": "python gets executed. One such concept is Bytecode",
+      "price": 47.69,
+      "rating": 4.5,
+      "review": 500
+    },
+    {
+      "img": "https://i.ibb.co/qBZ1qYs/10.jpg",
+      "title": "Full Stack Web Development",
+      "desc": "Build a complete web product & Earn an Coding Club",
+      "price": 48.69,
+      "rating": 4,
+      "review": 500
+    },
+    {
+      "img": "https://i.ibb.co/qBZ1qYs/10.jpg",
+      "title": "Advanced Database Queries",
+      "desc": "Learn the fundamentals of interacting with relatio",
+      "price": 133,
+      "rating": 4.2,
+      "review": 450
+    },
+    {
+      "img": "https://i.ibb.co/94KSR5h/11.png",
+      "title": "Cloud Application Develop",
+      "desc": "There is an app for everything, from online shopping",
+      "price": 176.4,
+      "rating": 4.8,
+      "review": 736
+    },
+    {
+      "img": "https://i.ibb.co/xH3ZJbt/12.jpg",
+      "title": "UX Design and Evaluation",
+      "desc": "It is hard to overstate the importance of UX",
+      "price": 1794.6,
+      "rating": 4.2,
+      "review": 450
+    },
+    {
+      "img": "https://i.ibb.co/QP67CjC/13.jpg",
+      "title": "Programming for the Web",
+      "desc": "JavaScript is the programming language of the World",
+      "price": 149,
+      "rating": 5,
+      "review": 862
+    },
+    {
+      "img": "https://i.ibb.co/9g2GTRG/15.jpg",
+      "title": "HTML5 Apps and Game",
+      "desc": "Want to learn advanced HTML5 tips and techniques?",
+      "price": 199,
+      "rating": 4.7,
+      "review": 862
+    },
+    {
+      "img": "https://i.ibb.co/C0BkBQf/16.jpg",
+      "title": "Building Modern Nodejs",
+      "desc": "In modern cloud native application development, it",
+      "price": 169,
+      "rating": 4.8,
+      "review": 286
+    },
+    {
+      "img": "https://i.ibb.co/pyxfzLg/17.jpg",
+      "title": "Introduction to Cloud Dev",
+      "desc": "Please Note: Learners who successfully complete",
+      "price": 49,
+      "rating": 4.5,
+      "review": 550
+    },
+    {
+      "img": "https://i.ibb.co/WG3f3G9/18.jpg",
+      "title": "Computer Science for Web",
+      "desc": "The web is a crucial part of our everyday lives.",
+      "price": 313.2,
+      "rating": 3.5,
+      "review": 280
+    },
+    {
+      "img": "https://i.ibb.co/ZdGjnjb/19.jpg",
+      "title": "Web Application Technology",
+      "desc": "In this course, you'll explore the basic structure",
+      "price": 49,
+      "rating": 2.2,
+      "review": 320
+    },
+    {
+      "img": "https://i.ibb.co/6nGYp4s/20.png",
+      "title": "Django for Everybody",
+      "desc": "This series introduces Python programmers to build",
+      "price": 176.4,
+      "rating": 3.5,
+      "review": 650
+    },
+    {
+      "img": "https://i.ibb.co/L96v3rt/21.png",
+      "title": "Programming for the Web",
+      "desc": "Learn how to develop dynamic, interactive",
+      "price": 113,
+      "rating": 3.5,
+      "review": 120
+    },
+    {
+      "img": "https://i.ibb.co/ThVZ42z/22.png",
+      "title": "Using JavaScript, JQuery,",
+      "desc": "This fourth and final course in Django for Everybody",
+      "price": 314.79,
+      "rating": 4.5,
+      "review": 450
+    },
+    {
+      "img": "https://i.ibb.co/sWXkDWq/23.png",
+      "title": "Exploring GraphQL: A Quer",
+      "desc": "Learn about GraphQL, an alternative to REST,",
+      "price": 188.9,
+      "rating": 4.2,
+      "review": 450
+    },
+    {
+      "img": "https://i.ibb.co/6ZSGRnZ/24.jpg",
+      "title": "Cloud Application Develop",
+      "desc": "You will understand the differences between Public",
+      "price": 128.69,
+      "rating": 3.5,
+      "review": 150
+    },
+    {
+      "img": "https://i.ibb.co/XtWGtFp/25.png",
+      "title": "HTML5 Apps and Games",
+      "desc": "Today, developers are increasingly moving from nat",
+      "price": 113,
+      "rating": 3,
+      "review": 290
+    },
+    {
+      "img": "https://i.ibb.co/gjwSMCn/2.png",
+      "title": "The Creative React",
+      "desc": "Build 3 Awesome Web Applications With React!",
+      "price": 29.99,
+      "rating": 4,
+      "review": 320
+    },
+    {
+      "img": "https://i.ibb.co/23gP8Jx/3.png",
+      "title": "The Creative Javascrip",
+      "desc": "From Beginner Javascript To Building Awesome Web Application",
+      "price": 133,
+      "rating": 4.2,
+      "review": 450
+    },
+    {
+      "img": "https://i.ibb.co/X8hfFh7/7.png",
+      "title": "The Creative HTML5 & CSS3",
+      "desc": "Create 3 amazing, well designed and animated website",
+      "price": 18.9,
+      "rating": 4,
+      "review": 680
+    },
+    {
+      "img": "https://i.ibb.co/xhDXHPP/4.png",
+      "title": "The Creative React and Re",
+      "desc": "Build 3 Awesome Web Applications With React!",
+      "price": 133,
+      "rating": 4.2,
+      "review": 450
+    },
+    {
+      "img": "https://i.ibb.co/RpwvfwR/5.png",
+      "title": "Top 100+ Python Interview",
+      "desc": "python gets executed. One such concept is Bytecode",
+      "price": 123,
+      "rating": 4.2,
+      "review": 450
+    },
+    {
+      "img": "https://i.ibb.co/60TN8fb/31.jpg",
+      "title": "Full Stack Web Development",
+      "desc": "Build a complete web product & Earn an Coding Club",
+      "price": 13,
+      "rating": 3.5,
+      "review": 670
+    },
+    {
+      "img": "https://i.ibb.co/T1Yr6NF/32.jpg",
+      "title": "Advanced Database Queries",
+      "desc": "Learn the fundamentals of interacting with relatioon",
+      "price": 133,
+      "rating": 4.2,
+      "review": 450
+    },
+    {
+      "img": "https://i.ibb.co/VW3LYc0/33.png",
+      "title": "Cloud Application Develop",
+      "desc": "There is an app for everything, from online shoppi",
+      "price": 133,
+      "rating": 4.2,
+      "review": 450
+    },
+    {
+      "img": "https://i.ibb.co/c3BvCpP/34.png",
+      "title": "UX Design and Evaluation",
+      "desc": "Programming for the Web",
+      "price": 113,
+      "rating": 5,
+      "review": 630
+    },
+    {
+      "img": "https://i.ibb.co/xhDXHPP/4.png",
+      "title": "CS50's Computer Science f",
+      "desc": "From Beginner Javascript To Building Awesome Web Application",
+      "price": 133,
+      "rating": 4.2,
+      "review": 450
+    }
+  ]
