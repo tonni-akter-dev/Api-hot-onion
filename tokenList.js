aafe5b36b1221b01b5a08f61f2c941a9ebff7b74
Tonni Akter <tonniakterbithi@gmaile.com>
2021-12-29 23:09:06 +0600

    s

--- tokenList.js
+++ tokenList.js
@@ -1,50 +1,50 @@
-[
+const tokenList = [
   {
-    "id": 0,
-    "title": "UNI",
-    "icon": "uniswap",
-    "tag": "uniswap"
+    id: 0,
+    title: "UNI",
+    icon: uniswap,
+    tag: "uniswap",
   },
   {
-    "id": 1,
-    "title": "RBC",
-    "icon": "polygon",
-    "tag": "rubic"
+    id: 1,
+    title: "RBC",
+    icon: polygon,
+    tag: "rubic",
   },
   {
-    "id": 2,
-    "title": "USDT",
-    "icon": "polygon",
-    "tag": "Tether USD"
+    id: 2,
+    title: "USDT",
+    icon: polygon,
+    tag: "Tether USD",
   },
   {
-    "id": 3,
-    "title": "BNB",
-    "icon": "uniswap",
-    "tag": "Binance Coin"
+    id: 3,
+    title: "BNB",
+    icon: uniswap,
+    tag: "Binance Coin",
   },
   {
-    "id": 4,
-    "title": "BTCB",
-    "icon": "polygon",
-    "tag": "BTCB Token"
+    id: 4,
+    title: "BTCB",
+    icon: polygon,
+    tag: "BTCB Token",
   },
   {
-    "id": 5,
-    "title": "XVS",
-    "icon": "polygon",
-    "tag": "venus"
+    id: 5,
+    title: "XVS",
+    icon: polygon,
+    tag: "venus",
   },
   {
-    "id": 6,
-    "title": "ULTRA",
-    "icon": "uniswap",
-    "tag": "Ultra Safe"
+    id: 6,
+    title: "ULTRA",
+    icon: uniswap,
+    tag: "Ultra Safe",
   },
   {
-    "id": 7,
-    "title": "RAY",
-    "icon": "polygon",
-    "tag": "radium"
-  }
-]
+    id: 7,
+    title: "RAY",
+    icon: polygon,
+    tag: "radium",
+  },
+];
