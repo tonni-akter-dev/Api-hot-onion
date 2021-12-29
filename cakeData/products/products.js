27968c64abc6126f3be0f2d03fcce560c187c70b
Tonni Akter <tonniakterbithi@gmaile.com>
2021-12-29 23:02:54 +0600

    s

---
+++ cakeData/products/products.js
@@ -0,0 +1,81 @@
+const products = [
+  {
+    img: "https://i.ibb.co/XjStyBP/2-2.jpg",
+    title: "Vanilla Cupcakes",
+    reviews: 5,
+    details:"Calf-length dress in airy, textured cotton fabric with a printed pattern Sed hendrerit. Cras risus ipsum, faucibus ut, ullamcorper id, varius estibulum ante ipsum primis in faucibus Product Details Inspired by traditional blockprinting techniques in India, our own in-house design is the vibrant pattern that every closet needs. That's why...",
+    categories: "Home page, New",
+    price:50.00
+  },
+  {
+    img: "https://i.ibb.co/dBsT6H6/2-1.jpghttps://i.ibb.co/khNQqbH/3.jpg",
+    title: "Delicious Cupcake Video",
+    reviews: 5,
+    details:
+      "Calf-length dress in airy, textured cotton fabric with a printed pattern Sed hendrerit. Cras risus ipsum, faucibus ut, ullamcorper id, varius estibulum ante ipsum primis in faucibus Product Details Inspired by traditional blockprinting techniques in India, our own in-house design is the vibrant pattern that every closet needs. That's why...",
+    categories: "Home page, New",
+    price:6.00
+
+  },
+  {
+    img: "https://i.ibb.co/rHwjX3p/2-3.jpg",
+    title: "Cake",
+    reviews: 5,
+    details:
+      "Calf-length dress in airy, textured cotton fabric with a printed pattern Sed hendrerit. Cras risus ipsum, faucibus ut, ullamcorper id, varius estibulum ante ipsum primis in faucibus Product Details Inspired by traditional blockprinting techniques in India, our own in-house design is the vibrant pattern that every closet needs. That's why...",
+    categories: "Home page, New",
+    price:6.00
+
+  },
+  {
+    img: "https://i.ibb.co/DDVsTQd/4.jpg",
+    title: "Chese Classic Burger",
+    reviews: 5,
+    details:
+      "Calf-length dress in airy, textured cotton fabric with a printed pattern Sed hendrerit. Cras risus ipsum, faucibus ut, ullamcorper id, varius estibulum ante ipsum primis in faucibus Product Details Inspired by traditional blockprinting techniques in India, our own in-house design is the vibrant pattern that every closet needs. That's why...",
+    categories: "Home page, New",
+    price:20.00
+
+  },
+  {
+    img: "https://i.ibb.co/DDVsTQd/4.jpg",
+    title: "Chocolate Mud Cake",
+    reviews: 5,
+    details:
+      "Calf-length dress in airy, textured cotton fabric with a printed pattern Sed hendrerit. Cras risus ipsum, faucibus ut, ullamcorper id, varius estibulum ante ipsum primis in faucibus Product Details Inspired by traditional blockprinting techniques in India, our own in-house design is the vibrant pattern that every closet needs. That's why...",
+    categories: "Home page, New",
+    price:30.00
+
+  },
+  {
+    img: "https://i.ibb.co/56GXdF7/5.jpg",
+    title: "Chocolate Mud Cake Affiliate",
+    reviews: 5,
+    details:
+      "Calf-length dress in airy, textured cotton fabric with a printed pattern Sed hendrerit. Cras risus ipsum, faucibus ut, ullamcorper id, varius estibulum ante ipsum primis in faucibus Product Details Inspired by traditional blockprinting techniques in India, our own in-house design is the vibrant pattern that every closet needs. That's why...",
+    categories: "Home page, New",
+    price:10.00
+
+  },
+  {
+    img: "https://i.ibb.co/s2Q8XWj/6.jpg",
+    title: "Sponge Cake Boosted Sale",
+    reviews: 5,
+    details:
+      "Calf-length dress in airy, textured cotton fabric with a printed pattern Sed hendrerit. Cras risus ipsum, faucibus ut, ullamcorper id, varius estibulum ante ipsum primis in faucibus Product Details Inspired by traditional blockprinting techniques in India, our own in-house design is the vibrant pattern that every closet needs. That's why...",
+    categories: "Home page, New",
+    price:12.00
+
+  },
+  {
+    img: "https://i.ibb.co/8DNMkL5/8.jpg",
+    title: "Chocolate Mud Cupcakes",
+    reviews: 5,
+    details:
+      "Calf-length dress in airy, textured cotton fabric with a printed pattern Sed hendrerit. Cras risus ipsum, faucibus ut, ullamcorper id, varius estibulum ante ipsum primis in faucibus Product Details Inspired by traditional blockprinting techniques in India, our own in-house design is the vibrant pattern that every closet needs. That's why...",
+    categories: "Home page, New",
+    price:15.00
+
+  }
+  
+];
