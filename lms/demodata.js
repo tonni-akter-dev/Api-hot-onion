775d68ca38c9ae357b734cdc1d35c0a864bce283
tonni-akter-dev <tonniakterbithi@gmail.com>
2022-12-03 15:02:29 +0600

    Library management data

---
+++ lms/demodata.js
@@ -0,0 +1,85 @@
+const demo = [
+    {
+        key: "000",
+        title: "Computer science, information & general works"
+    },
+    {
+        key: "001",
+        title: "Knowledge"
+    },
+    {
+        key: "002",
+        title: "The book"
+    },
+    {
+        key: "003",
+        title: "Systems"
+    },
+    {
+        key: "004",
+        title: "Data processing & computer science"
+    },
+    {
+        key: "005",
+        title: "Computer programming, programs & data"
+    },
+    {
+        key: "006",
+        title: "Special computer methods"
+    },
+    {
+        key: "007",
+        title: "[Unassigned]"
+    },
+    {
+        key: "008",
+        title: "[Unassigned]"
+    },
+    {
+        key: "009",
+        title: "[Unassigned]"
+    },
+    {
+        key: "010",
+        title: "Bibliography"
+    },
+    {
+        key: "011",
+        title: "Bibliographies"
+    },
+    {
+        key: "012",
+        title: "Bibliographies of individuals"
+    },
+    {
+        key: "013",
+        title: "[Unassigned]"
+    },
+    {
+        key: "014",
+        title: "Of anonymous & pseudonymous works"
+    },
+    {
+        key: "015",
+        title: "Bibliographies of works from specific places"
+    },
+    {
+        key: "016",
+        title: "Bibliographies of works on specific subjects"
+    },
+    {
+        key: "017",
+        title: "General subject catalogs"
+    },
+    {
+        key: "018",
+        title: "Catalogs arranged by author, date, etc."
+    },
+    {
+        key: "019",
+        title: "Dictionary catalogs"
+    },
+    {
+        key: "020",
+        title: "Library & information sciences"
+    }]
