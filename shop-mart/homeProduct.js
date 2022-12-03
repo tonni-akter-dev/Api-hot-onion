ab80b4dc483445a6683be3839bb7ec6321eb5e39
tonni-akter-dev <tonniakterbithi@gmail.com>
2022-12-03 14:59:22 +0600

    Library management data

--- shop-mart/homeProduct.js
+++ shop-mart/homeProduct.js
@@ -15,7 +15,8 @@ const all = [
             style: "CT5062-100",
             detailS: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled",
             color: "Green moss/White/Gray",
-            reviews: 5
+            reviews: 5,
+            category:"allPro"
         },
         {
             img: "https://i.ibb.co/r4wGhCr/5.jpg",
@@ -24,7 +25,8 @@ const all = [
             style: "CT5062-100",
             detailS: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled",
             color: "Green moss/White/Gray",
-            reviews: 5
+            reviews: 5,
+            category:"allPro"
         },
         {
             img: "https://i.ibb.co/TTZJCDT/6.jpg",
@@ -33,7 +35,8 @@ const all = [
             style: "CT5062-100",
             detailS: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled",
             color: "Green moss/White/Gray",
-            reviews: 5
+            reviews: 5,
+            category:"allPro"
         },
         {
             img: "https://i.ibb.co/8Nb10Jd/7.jpg",
@@ -42,7 +45,8 @@ const all = [
             style: "CT5062-100",
             detailS: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled",
             color: "Green moss/White/Gray",
-            reviews: 5
+            reviews: 5,
+            category:"allPro"
         },
         {
             img: "https://i.ibb.co/8Nb10Jd/7.jpg",
@@ -51,7 +55,8 @@ const all = [
             style: "CT5062-100",
             detailS: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled",
             color: "Green moss/White/Gray",
-            reviews: 5
+            reviews: 5,
+            category:"allPro"
         },
         {
             img: "https://i.ibb.co/99STsz8/8.jpg",
@@ -60,7 +65,8 @@ const all = [
             style: "CT5062-100",
             detailS: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled",
             color: "Green moss/White/Gray",
-            reviews: 5
+            reviews: 5,
+            category:"allPro"
         },
         {
             img: "https://i.ibb.co/n1ZNS9f/2.jpg",
@@ -69,7 +75,8 @@ const all = [
             style: "CT5062-100",
             detailS: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled",
             color: "Green moss/White/Gray",
-            reviews: 5
+            reviews: 5,
+            category:"allPro"
         },
         {
             img: "https://i.ibb.co/zQBfT9f/3.jpg",
@@ -78,6 +85,168 @@ const all = [
             style: "CT5062-100",
             detailS: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled",
             color: "Green moss/White/Gray",
-            reviews: 5
+            reviews: 5,
+            category:"allPro"
+        },
+        // Shoes
+        {
+            img: "https://i.ibb.co/c36m74h/s1.jpg",
+            title: "Mens Leather Shoes",
+            price: "£99.49 - £114.92",
+            style: "CT5062-100",
+            detailS: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled",
+            color: "Green moss/White/Gray",
+            reviews: 5,
+            category:"shoes"
+        },
+        {
+            img: "https://i.ibb.co/r3CCwnv/s2.jpg",
+            title: "Jam by J&P",
+            price: "£99.49 - £114.92",
+            style: "CT5062-100",
+            detailS: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled",
+            color: "Green moss/White/Gray",
+            reviews: 5,
+            category:"shoes"
+        },
+        {
+            img: "https://i.ibb.co/LnbZZzH/s3.jpg",
+            title: "Saul Dress Shoe",
+            price: "£99.49 - £114.92",
+            style: "CT5062-100",
+            detailS: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled",
+            color: "Green moss/White/Gray",
+            reviews: 5,
+            category:"shoes"
+        },
+        {
+            img: "https://i.ibb.co/JH98Hp2/s4.jpg",
+            title: "Nude Leather High Heels",
+            price: "£99.49 - £114.92",
+            style: "CT5062-100",
+            detailS: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled",
+            color: "Green moss/White/Gray",
+            reviews: 5,
+            category:"shoes"
+        },
+        {
+            img: "https://i.ibb.co/xDTSZWb/s5.jpg",
+            title: "Black Leather High Heels",
+            price: "£99.49 - £114.92",
+            style: "CT5062-100",
+            detailS: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled",
+            color: "Green moss/White/Gray",
+            reviews: 5,
+            category:"shoes"
+        },
+        {
+            img: "https://i.ibb.co/7pKFBgF/s6.jpg",
+            title: "Leather High Heels",
+            price: "£99.49 - £114.92",
+            style: "CT5062-100",
+            detailS: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled",
+            color: "Green moss/White/Gray",
+            reviews: 5,
+            category:"shoes"
+        },
+        {
+            img: "https://i.ibb.co/2PBs5PL/s7.jpg",
+            title: "Facebook Jam by J&M",
+            price: "£99.49 - £114.92",
+            style: "CT5062-100",
+            detailS: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled",
+            color: "Green moss/White/Gray",
+            reviews: 5,
+            category:"shoes"
+        },
+        {
+            img: "https://i.ibb.co/kMpZD88/s8.jpg",
+            title: "Saul Dress Shoe",
+            price: "£99.49 - £114.92",
+            style: "CT5062-100",
+            detailS: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled",
+            color: "Green moss/White/Gray",
+            reviews: 5,
+            category:"shoes"
+        },
+       
+        {
+            img: "https://i.ibb.co/PgmzHmM/b1.jpg",
+            title: "Backpack small glacier",
+            price: "£99.49 - £114.92",
+            style: "CT5062-100",
+            detailS: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled",
+            color: "Green moss/White/Gray",
+            reviews: 5,
+            category:"bags"
+        },
+        {
+            img: "https://i.ibb.co/LJ0D7fb/b2.jpg",
+            title: "Canvas tote bag",
+            price: "£99.49 - £114.92",
+            style: "CT5062-100",
+            detailS: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled",
+            color: "Green moss/White/Gray",
+            reviews: 5,
+            category:"bags"
+        },
+        {
+            img: "https://i.ibb.co/Yd5LY7m/b3.jpg",
+            title: "A4 2WAY Plain Leather",
+            price: "£99.49 - £114.92",
+            style: "CT5062-100",
+            detailS: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled",
+            color: "Green moss/White/Gray",
+            reviews: 5,
+            category:"bags"
+        },
+        {
+            img: "https://i.ibb.co/KssHPSx/b4.jpg",
+            price: "Galleria mini tote bag",
+            style: "CT5062-100",
+            detailS: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled",
+            color: "Green moss/White/Gray",
+            reviews: 5,
+            category:"bags"
+        },
+        {
+            img: "https://i.ibb.co/8NN98Bb/b5.jpg",
+            title: "Galleria mini tote bag",
+            price: "£99.49 - £114.92",
+            style: "CT5062-100",
+            detailS: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled",
+            color: "Green moss/White/Gray",
+            reviews: 5,
+            category:"bags"
+        },
+        {
+            img: "https://i.ibb.co/yscwdSn/b6.jpg",
+            title: "A4 2WAY Plain Leather",
+            price: "£99.49 - £114.92",
+            style: "CT5062-100",
+            detailS: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled",
+            color: "Green moss/White/Gray",
+            reviews: 5,
+            category:"bags"
+        },
+        {
+            img: "https://i.ibb.co/Qn2W7kQ/b7.jpg",
+            title: "Serpenti forever charm",
+            price: "£99.49 - £114.92",
+            style: "CT5062-100",
+            detailS: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled",
+            color: "Green moss/White/Gray",
+            reviews: 5,
+            category:"bags"
+        },
+        {
+            img: "https://i.ibb.co/3yvP3gc/b8.jpg",
+            title: "Princess Handbag",
+            price: "£99.49 - £114.92",
+            style: "CT5062-100",
+            detailS: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled",
+            color: "Green moss/White/Gray",
+            reviews: 5,
+            category:"bags"
         },
     ]
